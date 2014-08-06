1839f446743f05e6ebd626a4eac6508b55361fd4
YanxinXue <YanxinXue@gmail.com>
2014-08-06 22:55:05 +0800

    Add logs

--- checkvpn.sh
+++ checkvpn.sh
@@ -9,6 +9,8 @@ ERROR="[ERROR#${PID}]"
 
 echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") checkvpn.sh started" >> $LOG
 /jffs/pptp/run.sh
+#Wait vpn connect at 1st times
+sleep 180
 while true
 do
   if ping -c 1 192.168.100.1
@@ -28,16 +30,18 @@ do
         n=3
       else
         echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") Re-connect" >> $LOG
-        #Clean the system
         pidof vpnup.sh | xargs kill -s 9
         pidof vpndown.sh | xargs kill -s 9
+        pidof run.sh | xargs kill -s 9
         rm -f $LOCK
-        #Stop the VPN
+        echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") Kill shell script" >> $LOG
+        /jffs/pptp/vpndown.sh
+        sleep 60
+        echo "$INFO $(date "+%d/%b/%Y:%H:%M:%S") Add route back" >> $LOG
         /tmp/pptpd_client/vpn stop
-        sleep 15
-        #Restart system VPN
+        sleep 60
         /etc/config/pptpd_client.sh
-        sleep 15
+        sleep 60
         /jffs/pptp/run.sh
       fi
     fi
